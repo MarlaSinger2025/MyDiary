e06193354d73d99183bfab981bb997d8f5c740ed
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-06-11 13:27:15 +0200

    Added all neccessary components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import NewEntry from "./NewEntry";
+import Navigation from "./Navigation";
 
 const Header = () => {
     return (
         <header className="bg-base-100 shadow-sm">
             <div className="flex flex-1 flex-row justify-around">
             <h1 className="text-[#4bbea5] text-xl font-bold">My Diary</h1>
-            <NewEntry />
+            <Navigation />
             </div>
         </header>
     );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
-const NewEntry = () => {
+const Navigation = () => {
     return (
         <button className="btn btn-soft btn-accent">New Entry</button>
     );
 };
 
-export default NewEntry;
+export default Navigation;
