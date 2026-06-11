bd57a48e78593a7d15788c0bd8ae8ee1e316324e
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-06-11 14:01:23 +0200

    Added first EntryCard, needs styling and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import EntryOverview from "./components/EntryOverview";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="bg-slate-600 text-white min-h-screen">
       <Header />
+      <EntryOverview />
     </div>
   )
 };
---
+++ src/components/EntryOverview.jsx
@@ -0,0 +1,11 @@
+import EntryCard from "./EntryCard";
+
+const EntryOverview = () => {
+    return (
+        <div className='flex justify-center flex-wrap gap-4 p-4 w-full'>
+            <EntryCard />
+        </div>
+    );
+};
+
+export default EntryOverview;
