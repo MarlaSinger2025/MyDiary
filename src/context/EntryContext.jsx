33d8fcfd2144614d60c982bdd3ceab0ef25856be
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-06-14 14:28:45 +0200

    Added EntryContext logic

--- src/context/EntryContext.jsx
+++ src/context/EntryContext.jsx
@@ -1,13 +1,50 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const EntryContext = createContext();
 
-export default function EntryProvider({children}) {
+export const EntryProvider = ({children}) => {
 
-    const []
-}
+    const [entries, setEntries] = useState(() => {  // Load entries from localStorage only once (instead of using useEffect)
+
+    
+        const storedEntries = localStorage.getItem('entries');
+
+        return storedEntries
+        ? JSON.parse(storedEntries)
+        : [];
+        
+});
+
+    useEffect(() => {  //Save entries whenever they change
+         localStorage.setItem('entries', JSON.stringify(entries));
+        }, [entries]);
+
+    const addEntry = (entry) => {
+        const newEntry = {
+            id: crypto.randomUUID(),
+            ...entry,
+        };
+
+        setEntries((prevEntries) => [
+            newEntry,
+            ...prevEntries,
+        ]);
+    };
+
+    return (
+        <EntryContext.Provider 
+            value= {{
+                entries,
+                addEntry,
+            }}
+            >
+                {children}
+            </EntryContext.Provider>
+    );
+};
 
 // eslint-disable-next-line react-refresh/only-export-components
 export function useEntry() {
+
     return useContext(EntryContext);
-}
+};
